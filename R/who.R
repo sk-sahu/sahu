fd6fe802dad1e0b7bce268fa3b2e4505e1300f92
Sangram Keshari Sahu <sk-sahu@github.com>
2019-03-01 18:44:21 +0530

    Initial push

---
+++ R/who.R
@@ -0,0 +1,19 @@
+#' Who
+#
+#' This function will tail details about the author
+#' @param blank Keep the function argument blank
+#'
+#' @return this returns name
+#'
+#' @author Sangram, \email{sangramsahu15@gmail.com}
+#' @references \url{https://sksahu.net}
+#' @seealso \code{\link{who}}
+#' @keywords initial function
+#'
+#' @examples
+#' who()
+#'
+#' @export
+who <- function() {
+  print("Sangram Keshari Sahu")
+}
---
+++ tests/testthat/test_who.R
@@ -0,0 +1,5 @@
+test_that("Testing who function", {
+
+  expect_match("Sangram Keshari Sahu", "Sangram")
+
+})
