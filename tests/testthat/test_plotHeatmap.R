b2ed8251132017e4e89835ee39f5afdb41bfe75b
sk-sahu <sk-sahu@github.com>
2019-09-24 14:23:18 +0530

    test edited

--- tests/testthat/test_plotHeatmap.R
+++ tests/testthat/test_plotHeatmap.R
@@ -2,5 +2,6 @@ load(file.path("test_data.RData"))
 
 test_that('testing pheatmap graphics', {
   file <- system.file("extdata", "pasilla_gene_counts.csv", package = "sahu")
-  expect_doppelganger(plotHeatmap(file), my_heatmap)
+  out <- plotHeatmap(file)
+  vdiffr::expect_doppelganger("static heatmap", out)
 })
