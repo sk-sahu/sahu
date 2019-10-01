ee5f42b865e2ce8421a2b7aa80423189671eca83
sk-sahu <sk-sahu@github.com>
2019-10-02 00:55:41 +0530

    azure changes

--- tests/testthat/test_getGeneinfo.R
+++ tests/testthat/test_getGeneinfo.R
@@ -1,4 +1,4 @@
-load(file.path("test_data.RData"))
+readRDS(file.path("test_data.RData"))
 
 test_that('Testing if ENSEMBL API returns same dimention table', {
   expect_true(is.data.frame((getGeneinfo("ENSG00000157764"))))
